e5efd54ba1444cf1a6b9d2eed37390ed6dd0d9b7
jedelson <edelson.m.jason@gmail.com>
2018-07-25 13:12:20 -0700

    components converted to stateful classes
    for testing lifecycle hooks

--- first-react-app/src/Components/Persons/Person/Person.js
+++ first-react-app/src/Components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 import Radium from 'radium';
 // function person(){
@@ -12,21 +12,36 @@ import Radium from 'radium';
 //   return <p>I am a react person and i am {Math.floor(Math.random()*30)} years old</p>
 // }
 
-const person = (props) =>{
-  const style = {
-    '@media (min-width: 500px)':{
-      width:'450px'
-    }
+// const person = (props) =>{
+//   const style = {
+//     '@media (min-width: 500px)':{
+//       width:'450px'
+//     }
+//   }
+//
+//   return (
+//     <div className = {classes.Person} >
+//       <p onClick={props.click}>I'm {props.name} and i am {props.age} years old</p>
+//       <p>{props.children}</p>
+//       {/* onChange updates name value, value shows original value of name */}
+//       <input type="text" onChange= {props.changed} value = {props.name} ></input>
+//     </div>
+//   )
+// }
+class Person extends Component{
+  render(){
+    return (
+      <div className = {classes.Person} >
+        <p onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age} years old</p>
+        <p>{this.props.children}</p>
+        {/* onChange updates name value, value shows original value of name */}
+        <input type="text" onChange= {this.props.changed} value = {this.props.name} ></input>
+      </div>
+    )
   }
-
-  return (
-    <div className = {classes.Person} >
-      <p onClick={props.click}>I'm {props.name} and i am {props.age} years old</p>
-      <p>{props.children}</p>
-      {/* onChange updates name value, value shows original value of name */}
-      <input type="text" onChange= {props.changed} value = {props.name} ></input>
-    </div>
-  )
 }
+
+
+
 // for media style root is needed
-export default Radium(person);
+export default Radium(Person);
--- first-react-app/src/Components/Persons/Persons.js
+++ first-react-app/src/Components/Persons/Persons.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person.js';
 
-const persons = (props) => props.persons.map((person,index) =>{
-  return <Person
-    name={person.name}
-    age={person.age}
-    click={() => props.clicked(index)}
-    key={index} // should always be unique
-    changed={(event) => props.changed(event, index)}
-    ></Person>
+// const persons = (props) => props.persons.map((person,index) =>{
+//   return <Person
+//     name={person.name}
+//     age={person.age}
+//     click={() => props.clicked(index)}
+//     key={index} // should always be unique
+//     changed={(event) => props.changed(event, index)}
+//     ></Person>
+// }
+// );
+
+class Persons extends Component{
+  render(){
+    return this.props.persons.map((person,index) =>{
+      return <Person
+          name={person.name}
+          age={person.age}
+          click={() => this.props.clicked(index)}
+          key={index} // should always be unique
+          changed={(event) => this.props.changed(event, index)}
+          ></Person>
+    });
+  }
 }
-);
 
-
-export default persons
+export default Persons

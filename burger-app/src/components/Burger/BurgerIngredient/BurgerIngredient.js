8ef3018aae534385d88bb459e0e325764222d202
jedelson <edelson.m.jason@gmail.com>
2018-07-31 11:30:32 -0700

    static burger display

--- burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './burgerIngredient.css';
+import classes from './BurgerIngredient.css';
 
 const burgerIngredient = (props) => {
   let ingredient = null;

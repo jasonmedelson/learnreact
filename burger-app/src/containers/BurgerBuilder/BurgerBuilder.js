d49e8867b6a9fca5e8d1da85d398ddfdb83bd969
jedelson <edelson.m.jason@gmail.com>
2018-08-02 14:59:00 -0700

    ingredients saved in state

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../../hoc/Auxiliary.js';
 import Burger from '../../components/Burger/Burger.js'
 
 class BurgerBuilder extends Component{
+  state = {
+    ingredients:{
+      bacon:1,
+      salad:1,
+      cheese:1,
+      meat:1,
+    }
+  }
+
   render(){
     return(
       <Aux>
-        <Burger></Burger>
+        <Burger ingredients={this.state.ingredients}></Burger>
         <div>Build Controls</div>
       </Aux>
     );
